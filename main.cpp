f80898b874985c35e30ea046525bc4762d5796a5
Limerence0717 <10607664+limerence0717@user.noreply.gitee.com>
2025-02-14 10:32:22 +0800

    测试环境

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by LIUSHU on 2025/2/14.
+//
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}
---
+++ src/demo.cpp
@@ -0,0 +1,15 @@
+//
+// Created by LIUSHU on 2025/2/14.
+//
+
+//例题0000：a+b问题
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
